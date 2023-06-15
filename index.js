7059dbc5b569c97bf015068fd777a8e8bb685167
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2023-06-15 18:39:06 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const axios = require('axios');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 require('dotenv').config();
@@ -18,35 +17,56 @@ app.get('/', (req, res) => {
   });
 });
 
-app.get('/api/data', (req, res) => {
-  axios
-    // .get('http://192.168.0.151:5001/get')
-    .get('http://192.168.0.196:8000/api/json-file/')
-    .then((response) => {
-      const data = response.data;
-      console.log(data);
-      res.json(data);
-    })
-    .catch((error) => {
-      console.error(error);
-      res.status(500).json({ error: 'Failed to fetch data' });
-    });
-});
-
-app.get('/api/', (req, res) => {
+app.get('/api', (req, res) => {
   const data = [
     {
-      Date: '2023-06-15 15:58:21',
       id: 2,
       platenumber: '7414-08',
-      Date: '2023-06-15 15:58:21',
-      image: 'https://i.ibb.co/0jZ3qYH/IMG-20210615-155821.jpg',
+      hour: '15:58:21',
+      Date: '2023-06-15',
+      image: 'https://dummyimage.com/300',
+      plate_image: 'https://dummyimage.com/300',
     },
     {
-      Date: '2023-06-15 15:58:21',
       id: 3,
       platenumber: '7414-08',
-      Date: '2023-06-15 15:58:21',
+      hour: '15:58:21',
+      Date: '2023-06-15',
+      image: 'https://dummyimage.com/300',
+      plate_image: 'https://dummyimage.com/300',
+    },
+    {
+      id: 4,
+      platenumber: '7414-08',
+      hour: '20:58:21',
+      Date: '2023-06-15',
+      image: 'https://dummyimage.com/300',
+      plate_image: 'https://dummyimage.com/300',
+    },
+
+    {
+      id: 5,
+      platenumber: '7414-08',
+      hour: '21:38:21',
+      Date: '2023-03-15',
+      image: 'https://dummyimage.com/300',
+      plate_image: 'https://dummyimage.com/300',
+    },
+    {
+      id: 6,
+      platenumber: '7414-08',
+      hour: '12:23:21',
+      Date: '2023-06-11',
+      image: 'https://dummyimage.com/300',
+      plate_image: 'https://dummyimage.com/300',
+    },
+    {
+      id: 7,
+      platenumber: '7414-08',
+      hour: '08:20:21',
+      Date: '2023-05-05',
+      plate_image: 'https://dummyimage.com/300',
+      image: 'https://dummyimage.com/300',
     },
   ];
 
