94b8517b0f3f17d69fd60df89874bc863383ed5e
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2023-06-15 08:17:39 +0100

    changes

--- admin/src/pages/_app.js
+++ admin/src/pages/_app.js
@@ -1,14 +1,14 @@
-import Head from "next/head";
-import { CacheProvider } from "@emotion/react";
-import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
-import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
-import { CssBaseline } from "@mui/material";
-import { ThemeProvider } from "@mui/material/styles";
+import Head from 'next/head';
+import { CacheProvider } from '@emotion/react';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import { CssBaseline } from '@mui/material';
+import { ThemeProvider } from '@mui/material/styles';
 // import { AuthConsumer, AuthProvider } from 'src/contexts/auth-context';
-import { useNProgress } from "src/hooks/use-nprogress";
-import { createTheme } from "src/theme";
-import { createEmotionCache } from "src/utils/create-emotion-cache";
-import "simplebar-react/dist/simplebar.min.css";
+import { useNProgress } from 'src/hooks/use-nprogress';
+import { createTheme } from 'src/theme';
+import { createEmotionCache } from 'src/utils/create-emotion-cache';
+import 'simplebar-react/dist/simplebar.min.css';
 
 const clientSideEmotionCache = createEmotionCache();
 
@@ -27,12 +27,12 @@ const App = (props) => {
     <CacheProvider value={emotionCache}>
       <Head>
         <title>Truck Management</title>
-        <meta name="viewport" content="initial-scale=1, width=device-width" />
+        <meta name='viewport' content='initial-scale=1, width=device-width' />
       </Head>
       <LocalizationProvider dateAdapter={AdapterDateFns}>
         <ThemeProvider theme={theme}>
           <CssBaseline />
-
+          <SplashScreen />
           {getLayout(<Component {...pageProps} />)}
         </ThemeProvider>
       </LocalizationProvider>
--- admin/src/pages/input.jsx
+++ admin/src/pages/input.jsx
@@ -1,87 +1,365 @@
 import Head from 'next/head';
-import { subDays, subHours } from 'date-fns';
-import { Box, Container, Unstable_Grid2 as Grid } from '@mui/material';
+import {
+  Box,
+  Typography,
+  Paper,
+  Stack,
+  Select,
+  FormControl,
+  MenuItem,
+  InputLabel,
+  Button,
+  Unstable_Grid2 as Grid,
+} from '@mui/material';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
 import { Layout as DashboardLayout } from 'src/layouts/dashboard/layout';
-import { OverviewLatestOrders } from 'src/sections/overview/overview-latest-orders';
-import { OverviewLatestProducts } from 'src/sections/overview/overview-latest-products';
 import truck from '../../public/image 25.png';
 import screen from '../../public/screen.png';
 import Image from 'next/image';
+import { React, useState } from 'react';
 
-const now = new Date();
+const rows = [
+  createData('#50498', 'QY980945', '12/07/2023'),
+  createData('#107458', 'J8580945', 'Z695945', '04/07/2023'),
+  createData('#20658', 'Z695945', 'Z695945', '31/08/2023'),
+  createData('#23849', 'L87065', 'Z695945', '27/09/2023'),
+];
+
+function createData(ID, Plate_N, date) {
+  return { ID, Plate_N, date };
+}
 
 const Page = () => (
+  // const [age, setAge] = useState('');
+
+  // const handleChange = (event) => {
+  //   setAge(event.target.value);
+  // };
   <>
     <Head>
       <title>In put</title>
     </Head>
     <Box
-      component='main'
       sx={{
-        flexGrow: 1,
-        py: 8,
+        p: 2,
+        display: 'flex',
+        justifyContent: 'space-around',
+        bgcolor: '#D9D9D9',
+        height: '100%',
+        width: '100%',
       }}>
-      <Container maxWidth='xl'>
-        <Grid
-          sx={{
-            display: 'flex',
-            justifyContent: 'space-around',
-            alignItems: 'center',
-          }}
-          container
-          spacing={3}>
-          <Grid
-            item
-            xs={12}
-            md={6}
-            lg={6}
-            backgroundColor='#111C43'
-            borderRadius='12px'
-            width='291px'>
+      <Grid container spacing={3} sx={{ width: '100%' }}>
+        <Grid container item xs={12} md={6} lg={6}>
+          <Grid item xs={12} md={12} lg={12}>
             <Box
               sx={{
-                display: 'flex',
-                alignItems: 'center',
-                height: '100%',
+                border: 0.7 + 'rem solid',
+                borderColor: 'rgba(17, 28, 67, 1)',
                 width: '100%',
+                height: '47vh',
               }}>
-              <Image src={truck} alt='' width={423} height={291} />
+              <Image
+                style={{ objectFit: 'contain' }}
+                src={truck}
+                alt=''
+                width='100%'
+              />
             </Box>
           </Grid>
-
-          <Grid
-            item
-            xs={12}
-            sx={{ backgroundColor: 'gray', mt: 12 }}
-            md={6}
-            lg={6}
-            borderRadius='12px'>
+          <Grid item xs={12} md={12} lg={12}>
+            <Box
+              sx={{
+                bgcolor: 'rgba(17, 28, 67, 1)',
+                // height: '10px',
+                width: '100%',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}>
+              <Typography variant='body1' sx={{ color: 'white', py: 2 }}>
+                Du : 12:04:23 {'          '} l {'          '} A : 03:21:38
+              </Typography>
+            </Box>
+          </Grid>
+          <Grid item xs={12} md={12} lg={12}>
             <Box
               sx={{
                 display: 'flex',
                 justifyContent: 'center',
                 alignItems: 'center',
-                height: '100%',
                 width: '100%',
               }}>
-              <Image src={screen} alt='' />
+              <Paper elevation={1} sx={{ width: '100%', p: '20px' }}>
+                <Stack
+                  direction='row'
+                  justifyContent='space-between'
+                  alignItems='center'>
+                  <Typography variant='h6'> Input</Typography>
+                  <FormControl sx={{ width: '100px' }}>
+                    <InputLabel id='demo-simple-select-label'>
+                      Ce Mois
+                    </InputLabel>
+                    <Select
+                      labelId='demo-simple-select-label'
+                      id='demo-simple-select'
+                      // value={age}
+                      label='Age'
+                      // onChange={handleChange}
+                    >
+                      <MenuItem value={10}>Janvier</MenuItem>
+                      <MenuItem value={20}>Fevrier</MenuItem>
+                      <MenuItem value={30}>Mars</MenuItem>
+                    </Select>
+                  </FormControl>
+                </Stack>
+                <TableContainer sx={{ pt: '20px' }} component={Paper}>
+                  <Table sx={{ minWidth: 650 }} aria-label='simple table'>
+                    <TableHead>
+                      <TableRow>
+                        <TableCell>
+                          <Typography variant='body1' fontWeight='bold'>
+                            ID
+                          </Typography>
+                        </TableCell>
+                        <TableCell align='right'>
+                          <Typography variant='body1' fontWeight='bold'>
+                            Plate N
+                          </Typography>
+                        </TableCell>
+                        <TableCell align='right'>
+                          <Typography variant='body1' fontWeight='bold'>
+                            Date
+                          </Typography>
+                        </TableCell>
+                        <TableCell align='right'></TableCell>
+                      </TableRow>
+                    </TableHead>
+                    <TableBody>
+                      {rows.map((row) => (
+                        <TableRow
+                          key={row.ID}
+                          sx={{
+                            '&:last-child td, &:last-child th': { border: 0 },
+                          }}>
+                          <TableCell component='th' scope='row'>
+                            {row.ID}
+                          </TableCell>
+                          <TableCell align='right'>{row.Plate_N}</TableCell>
+                          <TableCell align='right'>{row.date}</TableCell>
+                          <TableCell align='right'>
+                            <Button
+                              variant='contained'
+                              sx={{
+                                bgcolor: 'rgba(17, 28, 67, 1)',
+                                color: 'white',
+                              }}></Button>
+                          </TableCell>
+                        </TableRow>
+                      ))}
+                    </TableBody>
+                  </Table>
+                </TableContainer>
+              </Paper>
             </Box>
           </Grid>
-          <Box
-            sx={{
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'center',
-              alignSelf: 'center',
-              height: '37px',
-              width: '424px',
-              mt: '14px',
-              backgroundColor: 'gray',
-            }}>
-            <h1>1</h1>
-          </Box>
         </Grid>
-      </Container>
+
+        <Grid spacing={2} container item xs={12} md={6} lg={6}>
+          <Grid item xs={12} md={12} lg={12}>
+            <Stack
+              direction='row'
+              sx={{ height: '12vh' }}
+              spacing={2}
+              justifyContent='space-between'>
+              <Box
+                sx={{
+                  height: '100%',
+                  bgcolor: 'white',
+                  width: '100%',
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  flexDirection: 'column',
+                  borderRadius: '10px',
+                }}>
+                <Typography variant='body'> ID</Typography>
+                <Typography variant='h6'> #50498</Typography>
+              </Box>
+              <Box
+                sx={{
+                  height: '100%',
+                  bgcolor: 'white',
+                  width: '100%',
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  flexDirection: 'column',
+                  borderRadius: '10px',
+                }}>
+                <Typography variant='body'> ID</Typography>
+                <Typography variant='h6'> 5108-02</Typography>
+              </Box>
+              <Box
+                sx={{
+                  height: '100%',
+                  bgcolor: 'white',
+                  width: '100%',
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  flexDirection: 'column',
+                  borderRadius: '10px',
+                }}>
+                <Typography variant='body2'> Date</Typography>
+                <Typography variant='h6'> 12/07/2023</Typography>
+              </Box>{' '}
+              <Box
+                sx={{
+                  height: '100%',
+                  bgcolor: 'white',
+                  width: '100%',
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  flexDirection: 'column',
+                  borderRadius: '10px',
+                }}>
+                <Typography variant='body2'> Time</Typography>
+                <Typography variant='h6'> 12:04:23</Typography>
+              </Box>
+            </Stack>
+          </Grid>
+
+          <Grid item xs={12} md={12} lg={12}>
+            <Box sx={{ bgcolor: 'red' }}>
+              <Image
+                style={{ objectFit: 'contain' }}
+                src={screen}
+                alt=''
+                // width='100%'
+                height={300}
+              />
+            </Box>
+          </Grid>
+
+          <Grid item xs={12} md={12} lg={12}>
+            <Stack
+              sx={{ width: '100%', bgcolor: 'red', py: 2 }}
+              direction='row'
+              justifyContent='space-evenly'
+              alignItems='center'>
+              <Button
+                variant='contained'
+                sx={{
+                  bgcolor: 'rgba(17, 28, 67, 1)',
+                  color: 'white',
+                  width: '100px',
+                  height: '39px',
+                }}>
+                <Typography variant='body1'> Check</Typography>
+              </Button>
+
+              <Button
+                variant='contained'
+                sx={{
+                  bgcolor: 'rgba(17, 28, 67, 1)',
+                  width: '100px',
+                  height: '39px',
+                  color: 'white',
+                }}>
+                <Typography variant='body1'> Check</Typography>
+              </Button>
+            </Stack>
+          </Grid>
+
+          <Grid item xs={12} md={12} lg={12}>
+            <Box
+              sx={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                width: '100%',
+              }}>
+              <Paper elevation={1} sx={{ width: '100%', p: '20px' }}>
+                <Stack
+                  direction='row'
+                  justifyContent='space-between'
+                  alignItems='center'>
+                  <Typography variant='h6'> Output</Typography>
+                  <FormControl sx={{ width: '100px' }}>
+                    <InputLabel id='demo-simple-select-label'>
+                      Ce Mois
+                    </InputLabel>
+                    <Select
+                      labelId='demo-simple-select-label'
+                      id='demo-simple-select'
+                      // value={age}
+                      label='Age'
+                      // onChange={handleChange}
+                    >
+                      <MenuItem value={10}>Janvier</MenuItem>
+                      <MenuItem value={20}>Fevrier</MenuItem>
+                      <MenuItem value={30}>Mars</MenuItem>
+                    </Select>
+                  </FormControl>
+                </Stack>
+                <TableContainer sx={{ pt: '20px' }} component={Paper}>
+                  <Table sx={{ minWidth: 650 }} aria-label='simple table'>
+                    <TableHead>
+                      <TableRow>
+                        <TableCell>
+                          <Typography variant='body1' fontWeight='bold'>
+                            ID
+                          </Typography>
+                        </TableCell>
+                        <TableCell align='right'>
+                          <Typography variant='body1' fontWeight='bold'>
+                            Plate N
+                          </Typography>
+                        </TableCell>
+                        <TableCell align='right'>
+                          <Typography variant='body1' fontWeight='bold'>
+                            Date
+                          </Typography>
+                        </TableCell>
+                        <TableCell align='right'></TableCell>
+                      </TableRow>
+                    </TableHead>
+                    <TableBody>
+                      {rows.map((row) => (
+                        <TableRow
+                          key={row.ID}
+                          sx={{
+                            '&:last-child td, &:last-child th': { border: 0 },
+                          }}>
+                          <TableCell component='th' scope='row'>
+                            {row.ID}
+                          </TableCell>
+                          <TableCell align='right'>{row.Plate_N}</TableCell>
+                          <TableCell align='right'>{row.date}</TableCell>
+                          <TableCell align='right'>
+                            <Button
+                              variant='contained'
+                              sx={{
+                                bgcolor: 'rgba(17, 28, 67, 1)',
+                                color: 'white',
+                              }}></Button>
+                          </TableCell>
+                        </TableRow>
+                      ))}
+                    </TableBody>
+                  </Table>
+                </TableContainer>
+              </Paper>
+            </Box>
+          </Grid>
+        </Grid>
+      </Grid>
     </Box>
   </>
 );
