87ff8b49874a3d7d10cf6344003833d660de3a60
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2023-06-15 10:53:05 +0100

    Update _app.js

--- admin/src/pages/_app.js
+++ admin/src/pages/_app.js
@@ -4,13 +4,26 @@ import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 import { CssBaseline } from '@mui/material';
 import { ThemeProvider } from '@mui/material/styles';
-// import { AuthConsumer, AuthProvider } from 'src/contexts/auth-context';
 import { useNProgress } from 'src/hooks/use-nprogress';
 import { createTheme } from 'src/theme';
 import { createEmotionCache } from 'src/utils/create-emotion-cache';
 import 'simplebar-react/dist/simplebar.min.css';
+import {
+  Hydrate,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const clientSideEmotionCache = createEmotionCache();
+
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false, // default: true
+    },
+  },
+});
 
 const SplashScreen = () => null;
 
@@ -25,17 +38,21 @@ const App = (props) => {
 
   return (
     <CacheProvider value={emotionCache}>
-      <Head>
-        <title>Truck Management</title>
-        <meta name='viewport' content='initial-scale=1, width=device-width' />
-      </Head>
-      <LocalizationProvider dateAdapter={AdapterDateFns}>
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-          <SplashScreen />
-          {getLayout(<Component {...pageProps} />)}
-        </ThemeProvider>
-      </LocalizationProvider>
+      <QueryClientProvider client={queryClient}>
+        <Hydrate state={pageProps.dehydratedState}></Hydrate>
+        <Head>
+          <title>Truck Management</title>
+          <meta name='viewport' content='initial-scale=1, width=device-width' />
+        </Head>
+        <LocalizationProvider dateAdapter={AdapterDateFns}>
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <SplashScreen />
+            {getLayout(<Component {...pageProps} />)}
+          </ThemeProvider>
+        </LocalizationProvider>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </CacheProvider>
   );
 };
